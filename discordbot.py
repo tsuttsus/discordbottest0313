aca7165afaf966c7c419a10ed1724bdded61c0f4
tsuttsus <64194219+tsuttsus@users.noreply.github.com>
2021-03-13 22:53:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,12 +12,9 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.listen()
 async def on_message(message):
-    if message.content.startswith("おはよう"):
-        if client.user != message.author:
-            m = "おはようございます" + message.author.name + "さん！"
-            await message.channel.send(m)
+    print('one')
+
 '''
 @bot.command()
 async def ping(ctx):
