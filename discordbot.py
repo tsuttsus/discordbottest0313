674f14f4942c7885fdcd95176bc62b0c71917491
tsuttsus <brvotsuttsu@gmail.com>
2021-03-13 20:58:30 +0900

    newfile

--- discordbot.py
+++ discordbot.py
@@ -5,9 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-time=0
-s=0
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -16,13 +13,9 @@ async def on_command_error(ctx, error):
 
 @client.event
 async def on_message(message):
-    # 「おはよう」で始まるか調べる
     if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
         if client.user != message.author:
-            # メッセージを書きます
             m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
             await message.channel.send(m)
 
 @bot.command()
